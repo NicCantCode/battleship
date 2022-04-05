3ce474bfbea69db31f8c07a273f9a02464010da4
Nic Kibler <aphemra@gmail.com>
2022-04-05 01:13:58 -0400

    Implemented Draggable Utility Class
    Added a class that makes sprites with a 2D collider drag and droppable

---
+++ Assets/_Scripts/Utility/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private Vector3 _positionOffset;
+    private Camera _mainCamera;
+    
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+    
+    private void OnMouseDown()
+    {
+        _positionOffset = transform.position - GetMousePosition();
+    }
+    
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePosition() + _positionOffset;
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        return mousePosition;
+    }
+}
