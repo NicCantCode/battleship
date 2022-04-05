3880c53de976641fc55102639a166652706ee95f
Nic Kibler <aphemra@gmail.com>
2022-04-05 19:30:12 -0400

    Moved All Possible Child Logic to Ship Parent
    * Added a mathod for sending raycasts from each ship marker hole to determine which cells they are overlapping
    * Added debug method for printing the cell names that are being overlapped

--- Assets/_Scripts/Parent Scripts/Ship.cs
+++ Assets/_Scripts/Parent Scripts/Ship.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public abstract class Ship : MonoBehaviour
 {
+    private readonly Vector3 _direction = new Vector3(0,0,1);
+    
     private int _hitCounter;
     
     public bool IsShipHit { get; set; }
@@ -17,6 +19,46 @@ public abstract class Ship : MonoBehaviour
     public ShipOwner ShipOwner => shipOwner;
 
     protected abstract void SinkShip();
+
+    private void SendRaycasts(float thisRotation, Transform thisTransform, Vector3 thisPosition)
+    {
+        var origin = new Vector3();
+        var offset = (ShipSize - 1) * 0.25f;
+
+        switch (thisRotation)
+        {
+            case 90:
+            case 270:
+                origin = new Vector3(thisPosition.x, thisPosition.y - offset, thisPosition.z);
+                break;
+            case 0:
+            case 180:
+                origin = new Vector3(thisPosition.x - offset, thisPosition.y, thisPosition.z);
+                break;
+        }
+        
+        for (var i = 0; i < ShipSize; i++)
+        {
+            var hit = Physics2D.Raycast(origin, _direction, 1);
+            
+            if (hit.collider != null)
+                print("Hit " + hit.transform.name + " with " + name + " Ray " + (i + 1));
+            
+            //Debug.DrawRay(origin, _direction, Color.red);
+            
+            switch (thisRotation)
+            {
+                case 90:
+                case 270:
+                    origin.y += 0.5f;
+                    break;
+                case 0:
+                case 180:
+                    origin.x += 0.5f;
+                    break;
+            }
+        }
+    }
     
     public void MarkShipAsHit()
     {
@@ -29,5 +71,13 @@ public abstract class Ship : MonoBehaviour
         {
             SinkShip();
         }
+    }
+
+    public void PrintCellNamesOnOverlap()
+    {
+        var thisTransform = transform;
+        var thisPosition = thisTransform.position;
+        
+        SendRaycasts(thisTransform.rotation.eulerAngles.z, thisTransform, thisPosition);
     }
 }
