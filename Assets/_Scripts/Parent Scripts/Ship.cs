9734dc4ca5c4ee0c545c606801d5909e067e425b
Nic Kibler <aphemra@gmail.com>
2022-04-07 22:26:41 -0400

    Updated Ship Script
    * Ship script now holds a reference to whether the ship is placed or not
    * Ship script also keeps track of the cells that a ship is currently overlapping/occupying via a Queue
    * Queue is used to best keep track of the currently occupied cells via the Enqueue and Dequeue methods
    * Ship script also handles whether a ship is within the boundaries of the board or not and sets a flag indicating as such

--- Assets/_Scripts/Parent Scripts/Ship.cs
+++ Assets/_Scripts/Parent Scripts/Ship.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Ship : MonoBehaviour
@@ -6,12 +8,19 @@ public abstract class Ship : MonoBehaviour
     private readonly Vector3 _direction = new Vector3(0,0,1);
     
     private int _hitCounter;
+    private bool _isAboveCells;
     
     public bool IsShipHit { get; set; }
+    
+    private Queue<Cell> _occupiedCells;
+    public Queue<Cell> OccupiedCells => _occupiedCells;
+
+    [SerializeField] private bool _isPlaced;
+    public bool IsPlaced => _isPlaced;
 
     [SerializeField] private int shipSize;
     public int ShipSize => shipSize;
-
+    
     [SerializeField] private ShipType shipType;
     public ShipType ShipType => shipType;
 
@@ -19,6 +28,17 @@ public abstract class Ship : MonoBehaviour
     public ShipOwner ShipOwner => shipOwner;
 
     protected abstract void SinkShip();
+
+    private void Start()
+    {
+        _occupiedCells = new Queue<Cell>(shipSize);
+        _isPlaced = false;
+    }
+
+    public void SetIsPlaced(bool isPlaced)
+    {
+        _isPlaced = isPlaced;
+    }
 
     private void SendRaycasts(float thisRotation, Transform thisTransform, Vector3 thisPosition)
     {
@@ -40,11 +60,18 @@ public abstract class Ship : MonoBehaviour
         for (var i = 0; i < ShipSize; i++)
         {
             var hit = Physics2D.Raycast(origin, _direction, 1);
-            
+
             if (hit.collider != null)
-                print("Hit " + hit.transform.name + " with " + name + " Ray " + (i + 1));
-            
-            //Debug.DrawRay(origin, _direction, Color.red);
+            {
+                _isAboveCells = true;
+                if (_occupiedCells.Count == shipSize) _occupiedCells.Dequeue();
+                _occupiedCells.Enqueue(hit.transform.gameObject.GetComponent<Cell>());
+            }
+
+            if (hit.collider == null)
+            {
+                _isAboveCells = false;
+            }
             
             switch (thisRotation)
             {
@@ -73,11 +100,13 @@ public abstract class Ship : MonoBehaviour
         }
     }
 
-    public void PrintCellNamesOnOverlap()
+    public bool IsValidShipPosition(Queue<Cell> occupiedCellsToCheck)
     {
         var thisTransform = transform;
         var thisPosition = thisTransform.position;
         
         SendRaycasts(thisTransform.rotation.eulerAngles.z, thisTransform, thisPosition);
+        
+        return _isAboveCells && occupiedCellsToCheck.Count == occupiedCellsToCheck.Distinct().Count() && occupiedCellsToCheck.Count != 0;
     }
 }
