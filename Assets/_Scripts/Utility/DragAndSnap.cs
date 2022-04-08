1e4700ccd38fcc69e3aca2c4826ed6f04a6a1685
Nic Kibler <aphemra@gmail.com>
2022-04-07 22:22:22 -0400

    Altered Draggable Class
    * Renamed to DragAndSnap to better reflect new functionality
    * Added functionality for snapping ships to their respective grid positions
    * Added functionality for marking cells as being occupied when a ship is placed on top of them
    * Occupied cells will filter into a purpose-built List of cells for better comparison during gameplay
    * This class now holds all functionality for handling ship drag and drop and ship snap to grid

---
+++ Assets/_Scripts/Utility/DragAndSnap.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DragAndSnap : MonoBehaviour
+{
+    private Camera _mainCamera;
+    [SerializeField] private bool _isDraggable;
+    private bool _isValidShipPosition;
+
+    private Ship _thisShip;
+    private Vector3 _originalShipPosition;
+
+    private GameManager _gameManager;
+    
+    private void Awake()
+    {
+        _gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        _mainCamera = Camera.main;
+        _thisShip = GetComponent<Ship>();
+        _originalShipPosition = transform.position;
+        _isDraggable = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!_isDraggable) return;
+
+        _isValidShipPosition = _thisShip.IsValidShipPosition(_thisShip.OccupiedCells);
+
+        transform.position = GetMousePosition();
+        
+        if (Input.GetKeyDown(KeyCode.R)) Rotate();
+    }
+
+    private void OnMouseUp()
+    {
+        if (_isValidShipPosition)
+        {
+            ResetShipTransform(false);
+            var cellPosition = _thisShip.OccupiedCells.Peek().transform.GetChild(0).position;
+            var offset = (_thisShip.ShipSize - 1) * 0.25f;
+            var shipRotationEulerAnglesZ = transform.rotation.eulerAngles.z;
+
+            switch (shipRotationEulerAnglesZ)
+            {
+                case 90:
+                case 270:
+                    transform.position = new Vector3(cellPosition.x, cellPosition.y + offset, -0.5f);
+                    break;
+                case 0:
+                case 180:
+                    transform.position = new Vector3(cellPosition.x + offset, cellPosition.y, -0.5f);
+                    break;
+            }
+            _thisShip.SetIsPlaced(true);
+            MarkCellsOccupied(_thisShip.OccupiedCells.ToList());
+        }
+        else
+        {
+            ResetShipTransform(true);
+            _thisShip.SetIsPlaced(false);
+        }
+        
+    }
+
+    private void MarkCellsOccupied(List<Cell> occupiedCells)
+    {
+        foreach (var cell in occupiedCells)
+        {
+            cell.SetShipState(true);
+            _gameManager.AddShipBearingCell(cell);
+        }
+    }
+
+    private void ResetShipTransform(bool rotationReset)
+    {
+        transform.position = _originalShipPosition;
+        if (rotationReset) transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        var mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = -0.5f;
+        return mousePosition;
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0, 0, 90, Space.World);
+    }
+
+    public void ToggleDrag()
+    {
+        _isDraggable = !_isDraggable;
+    }
+}
