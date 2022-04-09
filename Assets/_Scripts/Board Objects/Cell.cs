32832db4c714df3bab5c4a8f36647a890af1e8fc
Nic Kibler <aphemra@gmail.com>
2022-04-09 01:59:28 -0400

    Altered Cell Object Functonality
    Mostly created new getters and setters for new variables used within the Ship and Manager classes

--- Assets/_Scripts/Board Objects/Cell.cs
+++ Assets/_Scripts/Board Objects/Cell.cs
@@ -7,18 +7,29 @@ public class Cell : MonoBehaviour
     [SerializeField] private Markers markers;
     
     private GameManager _gameManager;
+    private BoardManager _boardManager;
     private GameObject _cellMarker;
+    private ShipType _occupiedShipType;
+    private Ship _occupiedShip;
     private bool _hasMarker;
     private bool _hasShip;
+    private bool _isInPlayerBoard;
 
     [SerializeField] private Vector2 _gridLocation;
 
     private void Awake()
     {
+        _occupiedShipType = ShipType.NONE;
         _gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        _boardManager = GameObject.FindGameObjectWithTag("Board Manager").GetComponent<BoardManager>();
     }
 
-    private void SetMarker(MarkerType markerType)
+    public void DebugSetMarker()
+    {
+        SetMarker(MarkerType.HIT);
+    }
+
+    public void SetMarker(MarkerType markerType)
     {
         var thisTransform = transform;
         var thisPosition = thisTransform.position;
@@ -28,11 +39,11 @@ public class Cell : MonoBehaviour
         switch (markerType)
         {
             case MarkerType.HIT:
-                _cellMarker = Instantiate(markers.hitMarkerPrefab, new Vector3(thisPosition.x, thisPosition.y, -0.5f), Quaternion.identity, thisTransform);
+                _cellMarker = Instantiate(markers.hitMarkerPrefab, new Vector3(thisPosition.x, thisPosition.y, -0.7f), Quaternion.identity, thisTransform);
                 _hasMarker = true;
                 break;
             case MarkerType.MISS:
-                _cellMarker = Instantiate(markers.missMarkerPrefab, new Vector3(thisPosition.x, thisPosition.y, -0.5f), Quaternion.identity, thisTransform);
+                _cellMarker = Instantiate(markers.missMarkerPrefab, new Vector3(thisPosition.x, thisPosition.y, -0.7f), Quaternion.identity, thisTransform);
                 _hasMarker = true;
                 break;
             default:
@@ -44,6 +55,11 @@ public class Cell : MonoBehaviour
     {
         Destroy(_cellMarker);
         _hasMarker = false;
+    }
+
+    public bool GetHasMarker()
+    {
+        return _hasMarker;
     }
 
     public Vector2 GetGridLocation()
@@ -61,23 +77,51 @@ public class Cell : MonoBehaviour
         return _hasShip;
     }
 
-    public void SetShipState(bool hasShip)
+    public ShipType GetShipType()
+    {
+        return _occupiedShipType;
+    }
+
+    public void SetShipState(bool hasShip, ShipType shipType, Ship ship)
     {
         _hasShip = hasShip;
+        _occupiedShipType = shipType;
+        _occupiedShip = ship;
+    }
+
+    public void SetIsInPlayerBoard(bool state)
+    {
+        _isInPlayerBoard = state;
     }
 
     private void OnMouseDown()
     {
-        if (!_gameManager.GetPlaceMarkerState()) return;
+        if (!_gameManager.GetPlaceMarkerState() && _isInPlayerBoard) return;
         
         switch (_hasMarker)
         {
             case false:
-                SetMarker(_gameManager.GetMarkerType());
+                var result = _hasShip ? MarkerType.HIT : MarkerType.MISS;
+                SetMarker(result);
+                
+                if (_hasShip) _occupiedShip.MarkShipAsHit();
+                
+                print($"Player {result} at ({_gridLocation.x}, {_gridLocation.y})!");
+                
+                _boardManager.EnemyTurnStart();
                 return;
             case true:
-                RemoveMarker();
                 return;
         }
+    }
+
+    public Ship GetOccupyingShip()
+    {
+        return _occupiedShip;
+    }
+
+    public void SetOccupyingShip(Ship ship)
+    {
+        _occupiedShip = ship;
     }
 }
