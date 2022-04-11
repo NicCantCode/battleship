e63463f7a8caa602ac296b998d1c609b6d53bcf3
Nic Kibler <aphemra@gmail.com>
2022-04-11 05:51:43 -0400

    Moved Utility Methods From BoardManager to Utils
    * Moved the Shuffle and ToStack functions to their own Utils class
    * Made class static for ease of use in BoardManager class

---
+++ Assets/_Scripts/Utility/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    public static List<T> Shuffle<T> (List<T> listToShuffle)
+    {
+        for (var i = listToShuffle.Count - 1; i > 0; i--)
+        {
+            var randomIndex = Random.Range(0, i);
+            (listToShuffle[i], listToShuffle[randomIndex]) = (listToShuffle[randomIndex], listToShuffle[i]);
+        }
+
+        return listToShuffle;
+    }
+    
+    public static Stack<T> ToStack<T>(List<T> listToConvert)
+    {
+        var stack = new Stack<T>();
+        
+        foreach (var item in listToConvert)
+            stack.Push(item);
+
+        return stack;
+    }
+}
