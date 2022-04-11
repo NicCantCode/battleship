1922208068b63271469061d258ed067c165810ed
Nic Kibler <aphemra@gmail.com>
2022-04-11 05:46:49 -0400

    Changed Ship From Abstract To Normal Class
    * Removed all child ship classes
    * Moved logic from child ship classes to Ship class
    * Changed Ship class to a normal class from abstract class
    * Moved Ship script to Board Objects folder
    * Updated all ship prefabs to use new standalone Ship class

--- Assets/_Scripts/Board Objects/Ship.cs
+++ Assets/_Scripts/Board Objects/Ship.cs
@@ -1,22 +1,20 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public abstract class Ship : MonoBehaviour
+public class Ship : MonoBehaviour
 {
     private readonly Vector3 _direction = new Vector3(0,0,1);
     
     private int _hitCounter;
     private bool _isAboveCells;
     private GameManager _gameManager;
-    
-    public bool IsShipHit { get; set; }
-    
+    private LogManager _logManager;
+
     private Queue<Cell> _occupiedCells;
     public Queue<Cell> OccupiedCells => _occupiedCells;
 
-    [SerializeField] private bool _isPlaced;
+    private bool _isPlaced;
     public bool IsPlaced => _isPlaced;
 
     [SerializeField] private int shipSize;
@@ -28,16 +26,31 @@ public abstract class Ship : MonoBehaviour
     [SerializeField] private ShipOwner shipOwner;
     public ShipOwner ShipOwner => shipOwner;
 
-    protected abstract void SinkShip();
 
     private void Awake()
     {
         _gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        _logManager = GameObject.FindGameObjectWithTag("Log Manager").GetComponent<LogManager>();
         _occupiedCells = new Queue<Cell>(shipSize);
         _isPlaced = false;
     }
+    
+    private void SinkShip()
+    {
+        switch (ShipOwner)
+        {
+            case ShipOwner.ENEMY:
+                _logManager.LogMessage($"Enemy {shipType.ToString()} was sunk!", new Color(155f / 255f, 0, 0));
+                GetGameManagerReference().SubtractFromEnemyShipPool();
+                break;
+            case ShipOwner.PLAYER:
+                _logManager.LogMessage($"Player {shipType.ToString()} was sunk!", new Color(155f / 255f, 0, 0));
+                GetGameManagerReference().SubtractFromPlayerShipPool();
+                break;
+        }
+    }
 
-    public GameManager GetGameManagerReference()
+    private GameManager GetGameManagerReference()
     {
         return _gameManager;
     }
@@ -99,7 +112,6 @@ public abstract class Ship : MonoBehaviour
         if (_hitCounter + 1 > shipSize) return;
 
         _hitCounter++;
-        print($"Ship Hit! {_hitCounter}/{shipSize} Hits.");
 
         // If the number of hits equals the total size of the ship, it will sink.
         if (_hitCounter >= shipSize)
