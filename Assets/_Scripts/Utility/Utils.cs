320635685e3a2b37e0117e45c76d843879897e29
Nic Kibler <aphemra@gmail.com>
2022-04-16 09:09:40 -0400

    Added New Utils Method
    Method returns the battleship-centric alpha-numeric cell designator when given a Vector2 grid position

--- Assets/_Scripts/Utility/Utils.cs
+++ Assets/_Scripts/Utility/Utils.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 
 public static class Utils
 {
+    private static Dictionary<int, string> battleshipCharacterDictionary = new Dictionary<int, string> {
+        {0, "A"},
+        {1, "B"},
+        {2, "C"},
+        {3, "D"},
+        {4, "E"},
+        {5, "F"},
+        {6, "G"},
+        {7, "H"},
+        {8, "I"},
+        {9, "J"}
+};
+    
     public static List<T> Shuffle<T> (List<T> listToShuffle)
     {
         for (var i = listToShuffle.Count - 1; i > 0; i--)
@@ -22,5 +35,10 @@ public static class Utils
             stack.Push(item);
 
         return stack;
+    }
+
+    public static string GridPositionToBattleshipPositionAsString(Vector2 gridPosition)
+    {
+        return $"{battleshipCharacterDictionary[(int) gridPosition.x]}{10 - gridPosition.y}";
     }
 }
